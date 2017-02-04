dd2934e2178707a0a7beb6951fefa4ab2fd240d4
Philippe Aucazou <paucazou@yahoo.fr>
2017-02-04 16:02:47 +0100

    Added new buttons

--- programme/web/kalendarium/forms.py
+++ programme/web/kalendarium/forms.py
@@ -7,6 +7,10 @@ class RechercheSimple(forms.Form):
     date_seule = forms.DateField(widget = forms.SelectDateWidget(years=range(1600,4101)),required = False,label="Choisissez une date : ",initial = datetime.date.today) #,attrs={"value":datetime.date.today()})
 
 class RechercheMotClef(forms.Form):
-    annee = forms.Select(choices = range(1600,4101)) #,required=True) #,initial=datetime.date.today().year)
+    annees=[]
+    for a in range(1600,4101):
+        annees.append((a,a))
+    annee = forms.CharField(widget=forms.Select(choices = annees),required=False,initial=datetime.date.today().year)
     recherche = forms.CharField(label="Entrez vos mots-clefs",required=False)
+    plus = forms.BooleanField(help_text="Recherche large",required=False)
 
