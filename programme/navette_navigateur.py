bbcfeb5892de45a95fc1c9324ee5bb14c6a52846
Philippe Aucazou <paucazou@yahoo.fr>
2017-02-05 17:39:56 +0100

    Improved launching system

--- programme/navette_navigateur.py
+++ programme/navette_navigateur.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 # -*-coding:Utf-8 -*
-import threading
 import os
-import sys 
-import webbrowser
 import subprocess
-from time import sleep
+import sys
+import threading
+import webbrowser
 
 ### Thread classes ###
 
@@ -19,10 +18,15 @@ class Serveur(threading.Thread):
 #        exec(open('manage.py').read())
         subprocess.run(['./manage.py','runserver'])
         
+### Main ###
 primus = Serveur()
-
 primus.start()
-sleep(1)
+
+return_code = 1
+while return_code != 0:
+    """Waits until the server is on. 'nc' tries to connect to server at '8000' port. Works on *nix."""
+    return_code = subprocess.call(['nc','-vz','localhost','8000'])
+    
 webbrowser.open_new_tab('localhost:8000/kalendarium/accueil')
 print("Local server will be closed within 30 minutes")
 primus.join(1800)
