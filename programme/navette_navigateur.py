a540e9303fbd5e0804b81abdbf5ef536443c84db
Philippe Aucazou <paucazou@yahoo.fr>
2017-02-09 15:41:27 +0100

    Improved paths

--- programme/navette_navigateur.py
+++ programme/navette_navigateur.py
@@ -14,20 +14,14 @@ class Serveur(threading.Thread):
 
     def run(self):
         """Run by Thread"""
-        os.chdir("./web")
-        sys.argv = ["manage.py","runserver"]
-#        exec(open('manage.py').read())
+        chemin = os.path.dirname(os.path.abspath(__file__))
+        os.chdir(chemin + "/web") # mettre un abspath
         subprocess.run(['./manage.py','runserver'])
         
 ### Main ###
 primus = Serveur()
 primus.start()
 
-"""return_code = 1
-while return_code != 0:
-    Waits until the server is on. 'nc' tries to connect to server at '8000' port. Works on *nix.
-    return_code = subprocess.call(['nc','-vz','localhost','8000'])
-    """
 local = http.client.HTTPConnection("localhost",port=8000,timeout=1)
 local.connection = "off"
 while local.connection == "off":
@@ -39,8 +33,8 @@ while local.connection == "off":
     except ConnectionRefusedError:
         local.connection = "off"
     
-webbrowser.open_new_tab('localhost:8000/kalendarium/accueil')
-print("Local server will be closed within 30 minutes")
-primus.join(1800)
+webbrowser.open_new_tab('http://localhost:8000/kalendarium/accueil') # open_new_tab renvoie un bool ; essayer de faire le test avec lui, plutôt qu'avec un connexion http
+print("Local server will be closed within 30 minutes") # doesn't work
+primus.join(1.0)
 
 
