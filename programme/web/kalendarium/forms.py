61128e470b980fb34cc9e21514f0400f0db849cf
Philippe Aucazou <paucazou@yahoo.fr>
2017-02-09 15:53:27 +0100

    Changed min and max value for year

--- programme/web/kalendarium/forms.py
+++ programme/web/kalendarium/forms.py
@@ -4,19 +4,19 @@ from django import forms
 import datetime
 
 annees=[]
-for a in range(1600,4101):
+for a in range(1960,2100):
     annees.append((a,a))
     
 douze = ['janvier','février','mars','avril','mai','juin','juillet','août','septembre','octobre','novembre','décembre']
 
 class RechercheSimple(forms.Form):
-    date_seule = forms.DateField(widget = forms.SelectDateWidget(years=range(1600,4101)),required = True,label="Choisissez une date : ",initial = datetime.date.today) #,attrs={"value":datetime.date.today()})
+    date_seule = forms.DateField(widget = forms.SelectDateWidget(years=range(1960,2100)),required = True,label="Choisissez une date : ",initial = datetime.date.today) #,attrs={"value":datetime.date.today()})
 
 class RechercheMotClef(forms.Form):
-    annee = forms.IntegerField(widget=forms.Select(choices = annees),max_value=4100,min_value=1600,required=True,initial=datetime.date.today().year)
+    annee = forms.IntegerField(widget=forms.Select(choices = annees),max_value=1960,min_value=2100,required=True,initial=datetime.date.today().year)
     recherche = forms.CharField(label="Entrez vos mots-clefs",required=True)
     plus = forms.BooleanField(help_text="Recherche large",required=False)
 
 class MoisSeul(forms.Form):
-    annee = forms.IntegerField(widget=forms.Select(choices = annees),max_value=4100,min_value=1600,required=True,initial=datetime.date.today().year)
+    annee = forms.IntegerField(widget=forms.Select(choices = annees),max_value=1960,min_value=2100,required=True,initial=datetime.date.today().year)
     mois = forms.CharField(widget = forms.Select(choices = douze),required = True,initial = douze[datetime.date.today().month - 1])
