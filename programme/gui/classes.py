9f7578e67902637cc723ae13edd1ea971870f638
Philippe Aucazou <paucazou@yahoo.fr>
2017-02-17 09:37:32 +0100

    Added main file, and classes files for GUI

---
+++ programme/gui/classes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*-coding:Utf-8 -*
+
+from PyQt5.QtWidgets import QApplication, QComboBox, QDockWidget, QHBoxLayout, QMainWindow, QLabel
+
+def Main(QMainWindow):
+    """Main window"""
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        
+    def initUI(self):
+        pass
+    
---
+++ programme/gui/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# -*-coding:Utf-8 -*
+
+import classes
+import sys
+
+app = classes.QApplication(sys.argv)
+main = classes.Main()
+sys.exit(app.exec_())
