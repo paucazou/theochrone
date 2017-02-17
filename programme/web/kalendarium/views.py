2110238812e200b1878a4729222f8fc12646fa1d
Philippe Aucazou <paucazou@yahoo.fr>
2017-02-17 10:10:29 +0100

    Added doc for each functions

--- programme/web/kalendarium/views.py
+++ programme/web/kalendarium/views.py
@@ -15,7 +15,16 @@ def home(request,
          recherche_mot_clef=RechercheMotClef(None),recherche_simple=RechercheSimple(None),mois_entier=MoisEntier(None),mois_seul=False,
          debut=datetime.date.today(),fin=datetime.date.today(),
          mots_clefs='',plus=False,annee=datetime.date.today().year):
-    """A function which defines homepage""" 
+    """A function which defines homepage. It is also used by other pages to print common code. It takes many arguments :
+    - a request sent by client ;
+    - three GET forms whith None as default value  : recherche_mot_clef, recherche_simple, mois_entier ;
+    - mois_seul : a bool to define whether a complete month will be returned or not ;
+    - debut : a datetime.date for the older date ;
+    - fin : a datetime.date for the latest date ;
+    - mots_clefs : a string used for research ;
+    - plus : a bool used to know whether the research must be large, or not ;
+    - annee : the year ;
+    """ 
 
     with open('./data/samedi.pic','rb') as file:
         pic=pickle.Unpickler(file)
@@ -49,6 +58,7 @@ def home(request,
     return render(request,'kalendarium/accueil.html',locals())
 
 def mc_transfert(request):
+    """A function which takes the request argument (GET) and returns the home function with the results of a research by name"""
     recherche_mot_clef = RechercheMotClef(request.GET or None)
     if recherche_mot_clef.is_valid():
         mots_clefs = recherche_mot_clef.cleaned_data['recherche']
@@ -59,6 +69,7 @@ def mc_transfert(request):
         return home(request, recherche_mot_clef)
         
 def date_transfert(request):
+    """A function which takes the request arguments (GET) and returns the home function with the results of a research by date"""
     recherche_simple = RechercheSimple(request.GET or None)
     if recherche_simple.is_valid():
         date = recherche_simple.cleaned_data['date_seule']
@@ -67,6 +78,7 @@ def date_transfert(request):
         return home(request,recherche_simple=recherche_simple)
 
 def mois_transfert(request):
+    """A function which takes the request arguments (GET) and returns the home function with the results of a research of a complete month"""
     mois_entier = MoisEntier(request.GET or None)
     if mois_entier.is_valid():
         mois = mois_entier.cleaned_data['mois']
