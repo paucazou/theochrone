38b2bebfa76c703d65ab8c5b374bf87e15e3f8f7
Philippe Aucazou <paucazou@yahoo.fr>
2017-02-17 09:56:19 +0100

    Added doc for functions

--- programme/officia.py
+++ programme/officia.py
@@ -3,6 +3,12 @@
 import adjutoria, datetime
 
 def fabrique_an(debut,fin,ordo=1962,propre='romanus'):
+    """Function which creates a liturgical year emulation. It takes four arguments :
+    - debut : a datetime.date for the older date ;
+    - fin : a datetime.date for the latest date ;
+    - ordo : an integer to select which missal will be used ;
+    - propre : a string to select the proper.
+    It returns a dict, whose keys are datetime.date, and values are lists containing Fete classes. The dict will not contain Saturday feasts of the Virgin, nor feria days."""
     year = debut.year - 1
     Annee = {}
     while True:
@@ -17,6 +23,15 @@ def fabrique_an(debut,fin,ordo=1962,propre='romanus'):
     return Annee
 
 def inversons(mots_bruts,debut,fin,samedi,plus=False,langue='francais',exit=True):
+    """Function which returns a list of feasts matching with mots_bruts. It takes seven args:
+    - mots_bruts : a string for the research ;
+    - debut : a datetime.date for the older date ;
+    - fin : a datetime.date for the latest date ;
+    - samedi : the Saturday of the Virgin Fete ;
+    - plus : a bool to define whether the results will be larger or not ;
+    - langue : language used ;
+    - exit : a bool to define whether the system have to exit or not in case of error ;
+    """
     Annee = fabrique_an(debut,fin)
     if isinstance(mots_bruts,list):
         mots_bruts = [adjutoria.sans_accent(mot) for mot in mots_bruts]
