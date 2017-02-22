0ebeb74709371d9a30592512f5840188ae0d6b5c
Philippe Aucazou <paucazou@yahoo.fr>
2017-02-22 16:01:56 +0100

    Changed the whole app to implement translation on the fly

--- programme/gui/classes.py
+++ programme/gui/classes.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 # -*-coding:Utf-8 -*
 
-import messages
-from PyQt5.QtCore import pyqtSignal, pyqtSlot, QDate, Qt
+from PyQt5.QtCore import pyqtSignal, pyqtSlot, QCoreApplication, QDate, QLocale, Qt, QTranslator
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QAction, QApplication, QCalendarWidget, QComboBox, QDockWidget, QHBoxLayout, QMainWindow, QLabel, QLineEdit, QPushButton, QTableWidget, QTabWidget, QVBoxLayout, QWidget
+
+_ = QCoreApplication.translate # a name more convenient
 
 class ReTranslateBox():
     """A class which can contain only items with the retranslateUI function"""
@@ -40,53 +41,63 @@ class SuperTranslator():
         SuperTranslator.retranslateUI()"""
         for a in self.W:
             a.retranslateUI()
+            
+class App(QApplication):
+    def __init__(self, args):
+        QApplication.__init__(self, args)
+        self.translator = QTranslator()
+        self.installTranslator(self.translator)
+        self.translator.load(QLocale(),"gui",'.','./i18n','.qm') # TODO : sélection de la langue dans cet ordre : settings, locale, puis choix.
+        self.execute = Main()
 
-class Main(QMainWindow):
+class Main(QMainWindow,SuperTranslator):
     """Main window"""
     def __init__(self):
         """Function which initializes the main window"""
-        super().__init__()
+        QMainWindow.__init__(self)
+        SuperTranslator.__init__(self)
         self.actions()
         self.initUI()
         
-        self.onglets.tab1.cal.clicked[QDate].connect(self.useDate)
-        self.onglets.tab1.kw_bouton.clicked.connect(self.useKeyWord)
+        self.W.onglets.W.tab1.cal.clicked[QDate].connect(self.useDate)
+        self.W.onglets.W.tab1.kw_bouton.clicked.connect(self.useKeyWord)
         
     def menu(self):
         """A function which describes the menubar of the main window"""
-        _ = messages.main_window['menus']
         menubar = self.menuBar()
         
         # File menu
-        fileMenu = menubar.addMenu(_['file'])
-        fileMenu.addAction(self.exitAction)
+        self.fileMenu = menubar.addMenu('file')
+        self.fileMenu.addAction(self.exitAction)
         
         # Edit menu
-        editMenu = menubar.addMenu(_['edit'])
+        self.editMenu = menubar.addMenu('edit')
+        
+        # Language menu (change on the fly)
+        self.languageMenu = menubar.addMenu('language')
         
         # Help menu
-        helpMenu = menubar.addMenu(_['help'])
+        self.helpMenu = menubar.addMenu('help')
         
     def actions(self):
         """A function which defines actions in the main window"""
-        _ = messages.main_window['actions']
+        
         # Exit
-        self.exitAction = QAction(QIcon('icons/exit.png'),_['exit_name'],self)
+        self.exitAction = QAction(QIcon('icons/exit.png'),'exit_name',self)
         self.exitAction.setShortcut('Ctrl+Q')
-        self.exitAction.setStatusTip(_['exit_status'])
         self.exitAction.triggered.connect(self.close)
         
     def initUI(self):
         """A function which defines widgets and main features of the window"""
-        _ = messages.main_window['init']
+        
         # widgets
         # main widget
         tableau = QTableWidget()
         self.setCentralWidget(tableau)
         # widgets on the right
-        self.rightDock = QDockWidget(_['right_dock'],self)
-        self.onglets = Onglets()
-        self.rightDock.setWidget(self.onglets)
+        self.rightDock = QDockWidget('right_dock',self)
+        self.W.onglets = Onglets()
+        self.rightDock.setWidget(self.W.onglets)
         self.addDockWidget(Qt.RightDockWidgetArea,self.rightDock)
         
         # menu
@@ -98,74 +109,107 @@ class Main(QMainWindow):
         # main features
         self.setGeometry(400,400,1000,1000) # TODO centrer la fenêtre au démarrage
         self.setWindowTitle('Theochrone - ')
+        self.retranslateUI() # voir si on ne la met pas carrément dans l'app, qui hériterait elle aussi de SuperTranslator
         self.show()
+        
+    def retranslateUI(self):
+        SuperTranslator.retranslateUI(self)
+        
+        #menu
+        self.fileMenu.setTitle(_('Main','File'))
+        self.editMenu.setTitle(_('Main','Edit'))
+        self.languageMenu.setTitle(_('Main','Language'))
+        self.helpMenu.setTitle(_('Main','Help'))
+        
+        #actions
+        self.exitAction.setText(_('Main','Exit'))
+        self.exitAction.setStatusTip(_('Main','Exit the app'))
+        
+        #initUI
+        #widgets on the right
+        self.rightDock.setWindowTitle(_('Main','Research'))
+        
         
     def useDate(self,date):
         self.setWindowTitle('Theochrone - ' + date.toString())
         
     def useKeyWord(self):
-        keyword = self.onglets.tab1.keyword.text()
+        keyword = self.W.onglets.W.tab1.keyword.text()
         print(keyword)
         
-class Onglets(QWidget):
+class Onglets(QWidget,SuperTranslator):
     """A class for a tab widget"""
     
     def __init__(self):
-        super().__init__()
+        QWidget.__init__(self)
+        SuperTranslator.__init__(self)
         self.initUI()
         
        
     def initUI(self):
-        _ = messages.onglets['init']
+        
         # main widgets
         self.tabs = QTabWidget()
-        self.tab1 = Unique()
+        self.W.tab1 = Unique()
         self.tabPlus = QWidget()
         
-        self.tabs.addTab(self.tab1,"1")
+        self.tabs.addTab(self.W.tab1,"1")
         self.tabs.addTab(self.tabPlus,"+")
         
         self.layout = QHBoxLayout()
         self.layout.addWidget(self.tabs)
         self.setLayout(self.layout)
-        self.setStatusTip(_['status'])        
+        self.setStatusTip('status')  
         
-class Unique(QWidget):
+    def retranslateUI(self):
+        SuperTranslator.retranslateUI(self)
+        self.setStatusTip(_('Onglets','Research Widget'))
+        
+class Unique(QWidget,SuperTranslator):
     """A class wich defines a widget with two types of research : for one date and for key-words."""
     
     def __init__(self):
-        super().__init__()
+        QWidget.__init__(self)
+        SuperTranslator.__init__(self)
         self.initUI()
         
     def initUI(self):
-        _ = messages.unique['init']
+
         self.layout = QVBoxLayout()
         
-        cal_label = QLabel(_['cal_label'],self)
-        self.layout.addWidget(cal_label)
+        self.cal_label = QLabel('cal_label',self)
+        self.layout.addWidget(self.cal_label)
         
         self.cal = QCalendarWidget(self)
         self.cal.setGridVisible(True)
         self.cal.setFirstDayOfWeek(0)
         self.cal.setMinimumDate(QDate(1600,1,1))
         self.cal.setMaximumDate(QDate(4100,12,31))
-        #self.cal.clicked[QDate].connect(self.sendDate)
-        
+
         self.layout.addWidget(self.cal)
         self.layout.addStretch(1)
         
-        kw_label = QLabel(_['kw_label'],self)
-        self.layout.addWidget(kw_label)
+        self.kw_label = QLabel('kw_label',self)
+        self.layout.addWidget(self.kw_label)
         
         self.keyword = QLineEdit(self)
         self.layout.addWidget(self.keyword)
         
-        self.kw_bouton = QPushButton(_['kw_button'],self)
+        self.kw_bouton = QPushButton('kw_button',self)
         self.layout.addWidget(self.kw_bouton)
         
         self.layout.addStretch(2)
         
         self.setLayout(self.layout)
+        
+    def retranslateUI(self):
+        SuperTranslator.retranslateUI(self)
+        
+        self.cal_label.setText(_('Unique','Please select a date : '))
+        self.kw_label.setText(_('Unique',"Please enter keywords : "))
+        self.kw_bouton.setText(_('Unique','Launch keywords research'))
+        
+        self.cal.setLocale(QLocale())
         
         
         
