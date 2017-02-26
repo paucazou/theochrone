499b586c59d3a942c2a594d23eb3080bdbeb13eb
Philippe Aucazou <paucazou@yahoo.fr>
2017-02-26 16:57:38 +0100

    Added first test file

---
+++ test_theochrone.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*-coding:Utf-8 -*
+#Deus, in adjutorium meum intende
+
+"""This file is to test theochrone.py"""
+
+import os
+
+os.chdir('..')
+
+from programme import theochrone
+
+def test_earlydate():
+    pass
+
