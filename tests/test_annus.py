e1978a9f05a4ed0c5fe6d1a96915f884ffe82202
Philippe Aucazou <paucazou@yahoo.fr>
2017-03-30 21:05:14 +0200

    Added test_oncecalled

--- tests/test_annus.py
+++ tests/test_annus.py
@@ -21,5 +21,10 @@ def test_paques():
     for year in range(1600,4100):
         assert L.easter(year) == easter_dates.check_dates[year]
         
-        
-        
+def test_oncecalled():
+    mock_class = mock.MagicMock()
+    mock_class.once_called = []
+    mock_class.method.return_value=True
+    mock_class.method = annus.oncecalled(mock_class.method)
+    assert mock_class.method(mock_class) == True
+    assert mock_class.method(mock_class) == False
