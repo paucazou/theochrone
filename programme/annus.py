e027bad78f2f2c69a59396387172e20d660fda49
Philippe Aucazou <paucazou@yahoo.fr>
2017-03-31 19:32:51 +0200

    Made create_empty_year staticmethod

--- programme/annus.py
+++ programme/annus.py
@@ -144,8 +144,9 @@ class LiturgicalCalendar():
         date = datetime.date(year,1,1)
         for date, day in self.year_data[year].items():
             self._selection(day,date)
-        
-    def create_empty_year(self,year):
+    
+    @staticmethod
+    def create_empty_year(year):
         """A method which creates the skeleton of each year"""
         tmp = {}
         date=datetime.date(year,1,1)
