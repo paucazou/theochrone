aa83c2b929864fc56a784a2e472ee0ceb5aecefb
Philippe Aucazou <paucazou@yahoo.fr>
2017-08-14 15:38:24 +0200

    Added files and lines to put theochrone on web

--- programme/web/web/urls.py
+++ programme/web/web/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
+    url(r"^$", views.renvoi),
     url(r'^admin/', admin.site.urls),
     url(r"^kalendarium/",include("kalendarium.urls")),
 ]
---
+++ programme/web/web/views.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# -*-coding:Utf-8 -*
+#Deus, in adjutorium meum intende
+from django.shortcuts import redirect
+
+def renvoi(request):
+            return redirect('/kalendarium/accueil')
+
