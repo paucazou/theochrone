340df5a57bbbabb60a59b1a19ecc012a0f63802e
Philippe Aucazou <paucazou@yahoo.fr>
2017-08-30 23:03:27 +0200

    Widget static js & css added

---
+++ programme/web/spill/static/spill/widget.js
@@ -0,0 +1,23 @@
+var iwtheo; // iframe content window
+var ifratheo = document.getElementById("theowidget");
+var bsurl = "https://theochrone.ga";
+var surl = bsurl + "/spill/main";
+console.log(surl);
+
+// send message to server
+ifratheo.onload = function (){
+    console.log('loaded');
+    var iwtheo = ifratheo.contentWindow;
+    iwtheo.postMessage("God sees you",surl);
+    console.log('Message sent');
+};
+// listener
+window.addEventListener("message", function(event){
+    if (event.origin == bsurl){
+        if (event.data == "set container to 0px"){
+            document.getElementById("theocontainer").style.height = "0px";
+        } else {
+    document.getElementById("theocontainer").style.height = event.data+'px';
+        }
+    }
+}, false);
