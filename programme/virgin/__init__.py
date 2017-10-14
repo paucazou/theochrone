9b9f10f45dc04956aaf5583f90edd5646fc5bbc1
Philippe Aucazou <paucazou@yahoo.fr>
2017-10-14 19:15:33 +0200

    Database : abandon : too long

--- programme/virgin/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-# -*-coding:Utf-8 -*
-#Deus, in adjutorium meum intende
-
-import virgin.adapters
-import virgin.slaves
-import virgin.virgindb
--- programme/virgin/setup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-# -*-coding:Utf-8 -*
-#Deus, in adjutorium meum intende
-from distutils.core import setup
-from Cython.Build import cythonize
-
-setup(
-    ext_modules = cythonize("slaves.py")
-    )
