7714d8ef082febbf5a074d6884518bdbd646205c
Philippe Aucazou <paucazou@yahoo.fr>
2017-10-22 22:41:20 +0200

    Improved martyrology

--- programme/martyrology.py
+++ programme/martyrology.py
@@ -12,6 +12,7 @@ import os
 import pickle
 
 TextResult = collections.namedtuple("TextResult",("title","main","last_sentence"))
+TextRatio = collections.namedtuple("TextRatio",("month","day",'ratio'))
 
 class Martyrology:
     """The Roman Martyrology.
@@ -41,8 +42,6 @@ class Martyrology:
         i = date.day
         if date.month == 2 and date.day >= 25 and calendar.isleap(date.year): # leap years : 25, 26, 27, 28 & 29 of February
             i -=1
-        if date.day == 2 and date.month == 11 and date.isoweekday() == 7: # All faithful departed commemoration
-            
         base = self._get_data(language)['data'][date.month-1][i-1]
         
         if date.month == 11 and ((date.day == 2 and date.isoweekday() != 7) or (date.day == 3 and date.isoweekday() == 1)):
@@ -66,13 +65,20 @@ class Martyrology:
         requested language"""
         return self._get_data(language)['credits']
     
-    def find(tokens,lang,max_nb_returned=5):
+    def find(self,tokens,lang,max_nb_returned=5):
         """Look into the texts and returned
         a list of the texts matching best with tokens entered. Best first.
         Items of the list are TextResult objects.
+        tokens = a string with keywords
         lang = a string definining which language should be use.
         max_nb_returned = an int which specifies the max number
         of results returned"""
         results = []
-        return results
+        sort_function = (fuzz.partial_token_sort_ratio,fuzz.partial_ratio)[len(tokens.split()) == 1]
+        for i,month in enumerate(self._get_data(lang)['data']):
+            for j,day in enumerate(month):
+                day = ' '.join(day)
+                results.append(TextRatio(i,j,sort_function(tokens,day)))
+        results.sort(key=lambda item:item.ratio,reverse=True)
+        return results[:max_nb_returned] # TODO à finir : renvoyer les textes eux-mêmes
 
