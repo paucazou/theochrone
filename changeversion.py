76e8ba9c532df1ce6af03d34d315f3190f841480
Philippe Aucazou <paucazou@yahoo.fr>
2017-12-08 18:33:44 +0100

    Changeversion.py is now usable

---
+++ changeversion.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+# -*-coding:Utf-8 -*
+#Deus, in adjutorium meum intende
+"""Change version of the soft in different places"""
+
+import argparse
+import collections
+import re
+
+FileData = collections.namedtuple('FileData',('path','line','line_clue'))
+
+def change_version(new_version: str,path: str,line: str,line_clue: str) -> None:
+    """Put new version into line where line_clue is
+    in the file path"""
+    with open(path) as f:
+        file = f.read().split('\n')
+    new_file = []
+    for line_ in file:
+        if line_clue in line_:
+            line_ = line.format(new_version)
+        new_file.append(line_)
+    with open(path,'w') as f:
+        f.write('\n'.join(new_file))
+    return None
+
+def main(new_version: str) -> None:
+    if not re.match("^\d+\.\d+\.\d+$",new_version):
+        raise ValueError("The new version doesn't seem to follow the correct syntax for a new version.\nPlease enter a version like X.X.X where X is a number")
+
+    for file_item in files_data:
+        change_version(new_version,file_item.path,file_item.line,file_item.line_clue)
+    
+    return None
+
+files_data = (
+    FileData('programme/command_line.py',"""\tsystem.add_argument('--version', action='version',version='%(prog)s {}')""","""'--version', action='version',version='"""),
+    FileData('README.md','Latest version is {}','Latest version is')
+    )
+
+parser = argparse.ArgumentParser(description="Change version of the soft in different places")
+parser.add_argument("version",help="Change version number.")
+
+if __name__ == '__main__':
+    args=parser.parse_args()
+    main(args.version)
+    
+
+    
