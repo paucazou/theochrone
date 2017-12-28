28a0bb15ffb7e865079bdc7574ee78da6db8db84
Philippe Aucazou <paucazou@yahoo.fr>
2017-12-28 23:22:15 +0100

    Reorganization + added function to export data in ics

--- programme/exporter.py
+++ programme/exporter.py
@@ -5,33 +5,16 @@
 Theochrone results into other formats, such as csv,
 in order to be used in other softwares"""
 import annus
+import arrow
 import csv
+import ics
 import officia
 import textwrap
 
-def to_csv(start: int,stop: int,file_path: str,lang: str,
-           proper='romanus', ordo=1962, **options) -> bool:
-    """Turn data into a csv file from year start to year stop.
-    proper & ordo select data sent
-    options are named arguments with a bool as value
-    Valid options are : 'pal' (Pro Aliquibus Locis) # include martyrology ? TODO
-    Return True"""
-    # checking errors
-    valid_options = {'pal'}
-    if len(set(options).intersection(valid_options)) != len(options):
-        raise KeyError("{}\nAn invalid option has been introduced. Please use one of the valid options: {}".format(*options.keys(),*valid_options))
-    incorrect_values = [val for val in options.values() if not isinstance(val,bool)]
-    if incorrect_values:
-        raise ValueError("{}\nAn invalid value has been given in options. Please use only booleans.".format(incorrect_values[0]))
-    # formatting csv writer
-    fields = ['Subject','Description','Start Date','End Date']
-    csv_file = open(file_path,'w',newline='')
-    writer = csv.DictWriter(csv_file,fieldnames=fields,dialect='excel')
-    writer.writeheader()
-    # collecting data
-    liturgycal = annus.LiturgicalCalendar(proper=proper,ordo=ordo)
-    liturgycal(start,stop)
-    # writing data
+def _data_wrapper(feast, lang: str, rank: int) -> tuple:
+    """Extract data from feast, which is a Fete or Fete like object.
+    rank is the rank of the feast in the day
+    Return a tuple : name, description, start, end"""
     descriptions = {'fr':textwrap.dedent("""\
                     État : {}
                     Classe : {}
@@ -43,38 +26,102 @@ def to_csv(start: int,stop: int,file_path: str,lang: str,
     category = {"fr":['Temporal','Sanctoral']}
     classe = {"fr":[0,1,2,3,4,"Commémoraison","messe Pro Aliquibus Locis"]}
     no_info = '/'
-    for day in liturgycal:
-        for feast in day:
-            if not feast.pal or options.get('pal',False):
-                ## state of feast
-                if feast.omission:
-                    fstate = state[lang][-1]
-                elif feast.celebree:
-                    fstate = state[lang][0]
-                elif feast.commemoraison and feast.peut_etre_celebree:
-                    fstate = state[lang][2]
-                elif feast.peut_etre_celebree:
-                    fstate = state[lang][1]
-                else:
-                    fstate = state[lang][-2]
-                ## station
-                if feast.__dict__.get('station',{lang:None})[lang]:
-                    fstation = feast.station[lang]
-                else:
-                    fstation = no_info
-                ## category
-                if not feast.sanctoral and not feast.temporal:
-                    fcategory = no_info
-                else:
-                    fcategory = category[lang][feast.sanctoral]
-                # setting description
-                description = descriptions[lang].format(
+    ## state of feast
+    if feast.omission:
+        fstate = state[lang][-1]
+    elif feast.celebree:
+        fstate = state[lang][0]
+    elif feast.commemoraison and feast.peut_etre_celebree:
+        fstate = state[lang][2]
+    elif feast.peut_etre_celebree:
+        fstate = state[lang][1]
+    else:
+        fstate = state[lang][-2]
+    ## station
+    if feast.__dict__.get('station',{lang:None})[lang]:
+        fstation = feast.station[lang]
+    else:
+        fstation = no_info
+    ## category
+    if not feast.sanctoral and not feast.temporal:
+        fcategory = no_info
+    else:
+        fcategory = category[lang][feast.sanctoral]
+    # setting description
+    description = descriptions[lang].format(
                     fstate,
                     classe[lang][feast.degre],
                     fcategory,
                     officia.affiche_temps_liturgique(feast,lang),
                     feast.couleur, # TODO not good for other languages
                     fstation)
+    # dates
+    fdate = arrow.Arrow(feast.date.year,feast.date.month,feast.date.day)
+    fdate = fdate.shift(seconds = rank)
+    
+    return feast.nom[lang], description, fdate 
+
+def main(start: int,stop: int,file_path: str,lang: str,
+           proper='romanus', ordo=1962, file_ext='ics', **options) -> bool:
+    """Turn data into a file from year start to year stop.
+    proper & ordo select data sent
+    options are named arguments with a bool as value
+    Valid options are : 'pal' (Pro Aliquibus Locis) # include martyrology ? TODO
+    Return True"""
+    # checking errors
+    valid_options = {'pal'}
+    if len(set(options).intersection(valid_options)) != len(options):
+        raise KeyError("{}\nAn invalid option has been introduced. Please use one of the valid options: {}".format(*options.keys(),*valid_options))
+    incorrect_values = [val for val in options.values() if not isinstance(val,bool)]
+    if incorrect_values:
+        raise ValueError("{}\nAn invalid value has been given in options. Please use only booleans.".format(incorrect_values[0]))
+
+    types = { 'csv':_to_csv,
+            'ics':_to_ics}
+    if file_ext not in types:
+        raise ValueError("{} is not a valid type. Please select a valid one: {}".format(file_ext,*types.keys()))
+    # collecting data
+    liturgycal = annus.LiturgicalCalendar(proper=proper,ordo=ordo)
+    liturgycal(start,stop)
+    # dispatching to custom function
+    types[file_ext](file_path, lang, liturgycal, **options)
+    return True
+
+def _to_ics(file_path: str, lang: str,
+        liturgycal, **options) -> bool:
+    """Turn data into a ics file.
+    liturgycal is a liturgical calendar with requested start and stop
+    Return True"""
+    calendar = ics.Calendar()
+    for day in liturgycal:
+        for i, feast in enumerate(day):
+            if not feast.pal or options.get('pal',False):
+                event = ics.Event()
+                raw_data = _data_wrapper(feast,lang,i)
+                event.name = raw_data[0]
+                event.description = raw_data[1]
+                event.begin = raw_data[2]
+                calendar.events.append(event)
+    with open(file_path,'w') as f:
+        f.writelines(calendar)
+
+
+
+def _to_csv(file_path: str,lang: str,
+           liturgycal, **options) -> bool:
+    """Turn data into a csv file.
+    liturgycal is a liturgical calendar with requested start and stop
+    Return True"""
+    # formatting csv writer
+    fields = ['Subject','Description','Start Date','End Date']
+    csv_file = open(file_path,'w',newline='')
+    writer = csv.DictWriter(csv_file,fieldnames=fields,dialect='excel')
+    writer.writeheader()
+    # writing data
+    for day in liturgycal:
+        for i,feast in enumerate(day):
+            if not feast.pal or options.get('pal',False):
+                description = _data_wrapper(feast,lang,i)[1]
                 data = {'Subject':feast.nom[lang],
                         'Description':description,
                         'Start Date': feast.date.strftime('%x'), # not safe, cause it depends on locale # WARNING 
