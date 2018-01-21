96b37ec7e1c5c6df8a4dc2a4af5acc5e742e2238
Philippe Aucazou <paucazou@yahoo.fr>
2018-01-21 09:34:15 +0100

    Fixed bug

--- programme/web/help/models.py
+++ programme/web/help/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class HelpArticle(models.Model):
-    title = models.CharField()
+    title = models.CharField(max_length=30)
     parent = models.ForeignKey('self',on_delete=models.CASCADE)
     version_major = models.PositiveSmallIntegerField()
     version_minor = models.PositiveSmallIntegerField()
@@ -11,7 +11,7 @@ class HelpArticle(models.Model):
     published = models.BooleanField(default=False)
 
     text = models.TextField() # the text of the article
-    short_description = models.CharField()
+    short_description = models.CharField(max_length=100)
 
     def __str__(self):
         return self.title
