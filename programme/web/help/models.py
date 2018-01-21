853358c9f5e88836f674199c521f4a7d454fa33b
Philippe Aucazou <paucazou@yahoo.fr>
2018-01-21 07:09:05 +0100

    Added new class: HelpArticle

--- programme/web/help/models.py
+++ programme/web/help/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class HelpArticle(models.Model):
+    title = models.CharField()
+    parent = models.ForeignKey('self',on_delete=models.CASCADE)
+    version_major = models.PositiveSmallIntegerField()
+    version_minor = models.PositiveSmallIntegerField()
+    version_patch = models.PositiveSmallIntegerField()
+    modification_date = models.DateTimeField(auto_now=True)
+    published = models.BooleanField(default=False)
+
+    text = models.TextField() # the text of the article
+    short_description = models.CharField()
+
+    def __str__(self):
+        return self.title
