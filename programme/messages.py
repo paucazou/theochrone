31e16a35f2ba92c1c01c945264457b8a3f8090bc
Philippe Aucazou <paucazou@yahoo.fr>
2018-01-27 10:50:15 +0100

    Added tests for messages.MessagesTranslator

--- programme/messages.py
+++ programme/messages.py
@@ -12,43 +12,46 @@ gettext.install('messages',loc)
 
 args = args()
 
-class MessagesTranslator:
+class MessagesTranslator: # TEST
     """This class manages the translation of the messages on the fly.
     Please do not include strings related to the content of the Fete objects themselves"""
-    def __init__(self,langs, lang: str):
+    def __init__(self,langs, lang: str): # TEST
         """langs is a dict with str as keys matching with a gettext.translation"""
         self.current_lang = lang
         self.langs = langs
 
-    def __get__(self,attribute: str):
+    def __getattr__(self,attribute: str): # TEST
         """Unsafe way to get a translation.
         Uses the current_lang"""
-        if '_' + attribute not in self.__dict__:
+        real_attribute = '_' + attribute
+        if real_attribute not in self.__dict__:
             raise AttributeError("{} doesn't exist".format(attribute))
-        return _(self.__dict__['_' + attribute])
+        return _(self.__dict__[real_attribute]) if real_attribute not in ('_current_lang','_langs') else self.__dict__[real_attribute]
 
-    def __set__(self,attribute: str,value: str):
+    def __setattr__(self,attribute: str,value: str): # TEST
         """Set a value"""
         if attribute.startswith('_'):
             raise AttributeError("Please do not set attributes starting with `_`: {}".format(attribute))
         new_attribute = "_" + attribute
-        if new_attribute in self.__dict__:
+        if new_attribute in self.__dict__ and new_attribute not in ('_current_lang','_langs'):
             raise AttributeError("{} already exists".format(attribute))
         self.__dict__[new_attribute] = value
 
-    def get(message: str,lang: str):
+    def get(self,message: str,lang: str): # TEST
         """return a translation of the message in the requested lang"""
-        if lang != self.lang:
-            self.current_lang = lang
-            self.langs[lang].install() # TODO maybe save the translations in a dict when first loaded
+        if lang != self.current_lang:
+            self.current_lang = ('en',lang)[lang in self.langs]
+            self.langs.get(lang,self.langs['en']).install() # TODO maybe save the translations in a dict when first loaded
         return _(getattr(self,message))
 
-    @staticmethod
-    def markToTranslate(value):
-        """A static method only used to mark a string litteral
-        as translatable.
-        You can call it from instance or from class.
+    def markToTranslate(self,value: str,attribute: str) -> str: # TEST
+        """Mark a string literal to be translated
+        and save it into the object under the attribute name
+        `example = MessagesTranslator(langs,lang)
+        example.markToTranslate("Praise the Lord, o my heart",'praise')
+        print(example.praise) # print Praise the Lord, o my heart`
         Command-line: xgettext -kmarkToTranslate -j -o OUTPUT INPUT"""
+        self.__setattr__(attribute,value)
         return value
 
 def translated_messages(file_name,language=args.langue):
@@ -75,5 +78,4 @@ def translated_messages(file_name,language=args.langue):
     messages['officia'] = { 
             }
     return messages[file_name]
-
 
