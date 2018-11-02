0f0279f99bf2dcf3e987872f8b6770fa701bfa77
Philippe Aucazou <paucazou@yahoo.fr>
2018-11-02 06:45:54 +0100

    Added new functions: lower_valuable and a base function for greater valuable and lower valuable

--- programme/utils.py
+++ programme/utils.py
@@ -14,3 +14,37 @@ def full_of(elt,iterable)-> bool:
             return False
 
     return True
+
+def valuable_length(iterable) -> int:
+    """Return the length of an iterable
+    after removing the elements evaluated
+    to False
+    >>> valuable_length(["ok","no","","else"])
+    3
+    """
+    return len([elt for elt in iterable if elt])
+
+def greater_valuable(f,s) -> int:
+    """Takes two iterables and look
+    which one has the greatest length
+    and return it
+    """
+    return __compare_valuable(f,s,True)
+
+def lower_valuable(f,s) -> int:
+    """Similar to greater_valuable
+    but return the lower one
+    """
+    return __compare_valuable(f,s,False)
+
+def __compare_valuable(f,s,greater=True) -> int:
+    """Base function for greater and
+    lower valuable."""
+    flen = valuable_length(f)
+    slen = valuable_length(s)
+    comparison = flen.__gt__ if greater else flen.__lt__
+
+    if comparison(slen):
+        return flen
+    else:
+        return slen
