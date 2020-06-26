3ab7685f509de7174ab4b6683b7144cd72c21377
BenoitEirik <olafsergent@gmail.com>
2020-06-26 10:32:32 +0200

    Changes the basic structure of the MGUI

--- programme/gui_mobile/main_winqml.py
+++ programme/gui_mobile/main_winqml.py
@@ -11,16 +11,56 @@ sys.path.append(chemin)
 
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtQml import QQmlApplicationEngine
+from PyQt5.QtCore import pyqtSlot, pyqtProperty, QAbstractListModel, QModelIndex, Qt, QTranslator
+from translation import *
 
 import qml
 import images
 
-class App:
+class App(QApplication):
     def __init__(self, args):
-        app = QApplication(args)
-        engine = QQmlApplicationEngine()
-        engine.quit.connect(app.quit)
-        engine.load(chemin + "/qml/main.qml")
-        sys.exit(app.exec_())
+        QApplication.__init__(self, args)
 
-        
+        # set QML engine
+        self.engine = QQmlApplicationEngine()
+        self.engine.load(chemin + "/qml/main.qml")
+        self.engine.quit.connect(App.quit)
+
+        self.translator = QTranslator()
+        self.installTranslator(self.translator)
+        self.execute = Main(self,args)
+
+class Main(SuperTranslator):
+    def __init__(self, parent, args):
+        SuperTranslator.__init__(self)
+        self.parent = parent
+        self.processCommandLineArgs(args)
+
+    def processCommandLineArgs(self, args):
+        args, debut, fin = args
+        reverse, plus = args.INVERSE, args.plus
+
+
+
+
+"""
+class ListModel(QAbstractListModel):
+    def __init__(self, data, parent=None, *args):
+        QAbstractListModel.__init__(self, parent, *args)
+        self._data = data
+
+    def setData(self, index, value, role):
+        if not index.isValid():
+            return False
+        self._data[index.row()] = value
+        self.dataChanged.emit(index, index, [role])
+        return True
+
+    @pyqtSlot(int, str, result=bool)
+    def insert(self, row, value):
+        return self.setData(self.createIndex(row, 0), value, Qt.EditRole)
+
+    @pyqtSlot(str, result=bool)
+    def append(self, value):
+        return self.insert(self.count, value)
+"""
